afeeeaf503201606230248bae00cce0278ce2ec9
John Daniele Capili <jdcapili12@gmail.com>
2019-11-22 11:37:10 -0500

    55 jump game dfs approach

--- leetcode/55_jump game.py
+++ leetcode/55_jump game.py
@@ -18,47 +18,79 @@
 
 # REWORK
 
-class Solution:
-    def canJump(self, nums: List[int]) -> bool:
-        if len(nums) == 1: return True
-        table = [False] * len(nums)
-        table[0] = True
+# class Solution:
+#     def canJump(self, nums: List[int]) -> bool:
+#         if len(nums) == 1: return True
+#         table = [False] * len(nums)
+#         table[0] = True
         
-        for i in range(0,len(nums) - 1):
+#         for i in range(0,len(nums) - 1):
             
-            if table[i] == True:
-                jump = i + nums[i]
-                for j in range(i , i + nums[i] + 1):
-                    if j > len(table) - 1: break
-                    table[j] = True
-                    if table[-1] is True: return True    
+#             if table[i] == True:
+#                 jump = i + nums[i]
+#                 for j in range(i , i + nums[i] + 1):
+#                     if j > len(table) - 1: break
+#                     table[j] = True
+#                     if table[-1] is True: return True    
                         
 
         
-        return False
+#         return False
 
+
+# class Solution:
+#     def canJump(self, nums: List[int]) -> bool:
+#         if len(nums) == 1: return True
+#         if nums[0] == 0: return False
+#         table = [False] * len(nums)
+#         table[0] = True
+        
+#         for i in range(0,len(nums) - 1):
+#             if nums[i] >= len(nums) - 1 - i and table[i] is True: 
+#                 print('jump higher than range')
+#                 return True
+#             print(table)
+#             if table[i] == True:
+#                 jump = i + nums[i]
+#                 for j in range(i , i + nums[i] + 1):
+#                     if j > len(table) - 1: break
+#                     table[j] = True
+#                     if table[-1] is True: 
+                        
+#                         return True    
+                        
+
+        
+#         return False
 
 class Solution:
-    def canJump(self, nums: List[int]) -> bool:
-        if len(nums) == 1: return True
-        if nums[0] == 0: return False
-        table = [False] * len(nums)
-        table[0] = True
-        
-        for i in range(0,len(nums) - 1):
-            if nums[i] >= len(nums) - 1 - i and table[i] is True: 
-                print('jump higher than range')
-                return True
-            print(table)
-            if table[i] == True:
-                jump = i + nums[i]
-                for j in range(i , i + nums[i] + 1):
-                    if j > len(table) - 1: break
-                    table[j] = True
-                    if table[-1] is True: 
-                        
-                        return True    
-                        
+    def canJump(self, nums):
+        table = set()
 
-        
+
+        def jump(start,table):
+            
+            print(table)
+            jump_range = nums[start]
+            # jump to new idx
+            nxt = start + jump_range
+            print(start,nxt,jump_range,len(nums) -1)
+            # check value
+            if nxt >= (len(nums) -1): 
+                return True
+            elif nxt in table: return
+            else:
+                table.add(nxt)
+                while nxt > start:
+                    if jump(nxt,table): return True
+                    nxt -= 1
+        # print(table)
+        if jump(0,table): return True
         return False
+    
+test = Solution()
+test.canJump(
+[2,3,1,1,4])
+
+
+
