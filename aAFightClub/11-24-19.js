1a1ca51877cbf8174a34ddc9163035439842fe10
John Daniele Capili <jdcapili12@gmail.com>
2019-11-24 17:34:59 -0500

    promises lesson for fight club

--- aAFightClub/11-24-19.js
+++ aAFightClub/11-24-19.js
@@ -9,15 +9,42 @@
 // console.log('bye')
 // would still execute hi, bye, times up
 
-console.log('hi');
+// console.log('hi');
 
-timeout();
+// timeout();
 
-console.log('end')
+// console.log('end')
 
 
-function timeout(){
-    console.log('hey')
-    setTimeout(()=> console.log('waiting'), 1000)
-    console.log('hello')
-}
+// function timeout(){
+//     console.log('hey')
+//     setTimeout(()=> console.log('waiting'), 1000)
+//     console.log('hello')
+// }
+
+
+// PROMISE has 2 terminal states
+// - resolved
+// - rejected
+// Promise has 2 intermediate state
+// -pending
+
+// const task1 = new Promise((resolve, reject) =>{
+//     setTimeout(() => {
+//         console.log('bye');
+//         resolve(42);
+//     },1000)
+// });
+
+// task1.then(task2)//.catch(task3)
+
+// function task2(resolution){
+//     console.log('value is', resolution)
+//     console.log('two')
+// }
+
+// function task3(){
+//     console.log('three')
+// }
+
+// Always handle promise rejections
