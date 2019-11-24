0d8253995f06f31bd304df775b9d8a6863649675
John Daniele Capili <jdcapili12@gmail.com>
2019-11-24 15:39:22 -0500

    promises js

--- aAFightClub/11-24-19.js
+++ aAFightClub/11-24-19.js
@@ -1,5 +1,23 @@
 // review promises
 
-setTimeout(() => {
-    console.log('times up!')
-},1000)
+// console.log('hi')
+
+// setTimeout(() => {
+//     console.log('times up!')
+// },0)
+
+// console.log('bye')
+// would still execute hi, bye, times up
+
+console.log('hi');
+
+timeout();
+
+console.log('end')
+
+
+function timeout(){
+    console.log('hey')
+    setTimeout(()=> console.log('waiting'), 1000)
+    console.log('hello')
+}
