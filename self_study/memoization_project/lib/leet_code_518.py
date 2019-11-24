3b634aca54261e298899d8ce0033683bda88181d
John Daniele Capili <jdcapili12@gmail.com>
2019-11-24 14:23:29 -0500

    memoization leetcode 518

--- self_study/memoization_project/lib/leet_code_518.py
+++ self_study/memoization_project/lib/leet_code_518.py
@@ -1,20 +1,16 @@
-// Work through this problem on https://leetcode.com/problems/coin-change-2/ and use the specs given there.
-// Feel free to use this file for scratch work.
-
+# // Work through this problem on https://leetcode.com/problems/coin-change-2/ and use the specs given there.
+# // Feel free to use this file for scratch work.
+import collections
 class Solution:
-    def change(self, amount: int, coins: List[int], memo = None) -> int:
-        if memo is None: memo = {}
-
-        if amount == 0: return 1
-        if amount in memo: return memo[amount]
-
-        num_coins = []
+    def change(self, amount, coins, memo = None):
+        if memo is None: memo = collections.defaultdict(int)
+        memo[0] = 1
         for coin in coins:
-
-            if coin <= amount:
-                print(coin, amount - coin)
-        num_coins.append(self.change(amount - coin, coins, memo) + 1)
-
-        memo[amount] = len(num_coins)
-        print(memo)
-return memo[amount]
+            for i in range(coin, amount +1):
+                print(i,coin)
+                if i >= coin:
+                    memo[i] += memo[i-coin]
+        print(memo)            
+        return memo[amount]
+test = Solution()
+test.change(5, [1,2,5])
