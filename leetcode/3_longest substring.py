ca15819bf25588fb1cdfc7cca4d04bf4e8bd088a
John Daniele Capili <jdcapili12@gmail.com>
2019-11-25 17:35:16 -0500

    modified leetcode problem 3

--- leetcode/3_longest substring.py
+++ leetcode/3_longest substring.py
@@ -15,6 +15,18 @@ class Solution:
                 
         return len(longest)
 
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        curr = ""
+        max_len = 0
+        for char in s:
+            if char not in curr: curr += char
+            else:
+                max_len = max(len(curr), max_len)
+                start = curr.index(char)
+                curr = curr[start+1:]+char
+        return max(max_len, len(curr))
+
 test = Solution()
 test.lengthOfLongestSubstring(
 "anviaj")
