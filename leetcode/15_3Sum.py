150ffa28630389ebaaad9715805ed3672d01c554
John Daniele Capili <jdcapili12@gmail.com>
2019-11-25 17:20:43 -0500

    alternate solution

--- leetcode/15_3Sum.py
+++ leetcode/15_3Sum.py
@@ -15,3 +15,20 @@ class Solution:
                 else: end -= 1
                     
         return ans
+
+
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        unique = set()
+        for i in range(len(nums)):
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+            target = -nums[i]
+            dp = set()
+            for j in range(i + 1, len(nums)):
+                if target - nums[j] in dp:
+                    temp = tuple(sorted([nums[i], target - nums[j], nums[j]]))
+                    unique.add(temp)
+                dp.add(nums[j])
+        return unique
