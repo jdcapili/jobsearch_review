67647606b894b24cb5f44872ecac5729e64a7c9d
John Daniele Capili <jdcapili12@gmail.com>
2019-11-26 14:12:24 -0500

    set and get functions

--- nyc-coders/hash_tables.js
+++ nyc-coders/hash_tables.js
@@ -14,7 +14,25 @@ class HashTable{
         return total
     }
 
-    get(key){
+    set(key,value){
+        let idx = hashFunc(key)
+        // check if anything exists this.keymap[idx]
+        if(!this.keymap[idx]){
+            // if undefined, initialize array
+            // push [key, value]
+            this.keymap[idx] = []
+        }
+        this.keymap[idx].push([key, value]);
+    }
 
+    get(key){
+        let idx = hashFunc(key);
+        if(!this.keymap[idx]) return undefined
+        else{
+            this.keymap[idx].forEach((subarr) => {
+                if(subarr[0] === key) return subarr[1]
+            })
+            return undefined
+        }
     }
 }
