c8657505949086328eb120d5033c69c36452ab0c
John Daniele Capili <jdcapili12@gmail.com>
2019-11-29 13:00:42 -0500

    minor changes for hashtables.js

--- nyc-coders/hash_tables.js
+++ nyc-coders/hash_tables.js
@@ -20,10 +20,25 @@ class HashTable{
         if(!this.keymap[idx]){
             // if undefined, initialize array
             // push [key, value]
-            this.keymap[idx] = []
+            this.keymap[idx] = [];
+            this.keymap[idx].push([key,value]);
+            return value;
+        }else{
+
+            this.keymap[idx].forEach((subarr,i) => {
+                if(subarr[0] == key && subarr[1] != value){
+                    this.keymap[idx][i] = [key,value];
+                    return value;
+                } else if (subarr[0] == key && subarr[1] == value){
+                    return value;
+                } 
+            })
+
+            this.keymap[idx].push([key,value])
         }
         
         this.keymap[idx].push([key, value]);
+
     }
 
     get(key){
