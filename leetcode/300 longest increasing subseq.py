b4800daaf24f2a4a6e2b8f919f86c58aae7217b2
John Daniele Capili <jdcapili12@gmail.com>
2019-11-29 13:01:37 -0500

    modifications for leetcode 300

---
+++ leetcode/300 longest increasing subseq.py
@@ -0,0 +1,20 @@
+class Solution:
+    def lengthOfLIS(self, nums):
+        tails = [0] * len(nums)
+        size = 0
+        for x in nums:
+            i, j = 0, size
+            
+            while i != j:
+                m = (i + j) // 2
+                if tails[m] < x:
+                    i = m + 1
+                else:
+                    j = m
+            tails[i] = x
+            size = max(i + 1, size)
+            print(size,i,x,tails)
+        return size
+
+test = Solution()
+print(test.lengthOfLIS([10,9,2,5,3,1,7,101,18]))
