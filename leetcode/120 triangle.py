1f61eba62e1401163346ef800551ade83ebae9ab
John Daniele Capili <jdcapili12@gmail.com>
2019-12-14 18:27:43 -0500

    notes

--- leetcode/120 triangle.py
+++ leetcode/120 triangle.py
@@ -1,7 +1,7 @@
 class Solution:
     def minimumTotal(self, triangle: List[List[int]]) -> int:
         f = [0] * (len(triangle) + 1)
-        for row in triangle[::-1]:
+        for row in triangle[::-1]: # reversed iteration on the 2d array.
             for i in range(len(row)):
                 f[i] = row[i] + min(f[i], f[i + 1])
         return f[0]
