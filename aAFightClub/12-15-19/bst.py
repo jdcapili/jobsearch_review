8d774d2812b7be6b1ecd025a55fe54594e727e03
John Daniele Capili <jdcapili12@gmail.com>
2019-12-15 16:58:44 -0500

    bst class

--- aAFightClub/12-15-19/bst.py
+++ aAFightClub/12-15-19/bst.py
@@ -4,37 +4,80 @@ class Node:
         self.left = None
         self.right = None
 
-a = Node(10)
-b = Node(5)
-c = Node(15)
-d = Node(3)
-e = Node(7)
-f = Node(17)
+class BST:
+    def __init__(self):
+        self.root = None
+    
+    def insert(self,val):
+        if self.root is None: 
+            self.root = Node(val)
+            return self.root
 
-a.left = b
-a.right = c
-b.left = d
-b.right = e
-c.right = f
+        current = self.root
+        while True:
+            if val >= current.val:
+                if current.right is None:
+                    current.right = Node(val)
+                    return current.right
+                else:
+                    current = current.right
+            else:
+                if current.left is None:
+                    current.left = Node(val)
+                    return current.left
+                else:
+                    current = current.left
+    
+    def binary_search_iter(self,val):
+        current = self.root
+        while current is not None:
+            if current.val == val: return True
+            elif val < current.val: current = current.left
+            else: current = current.right
 
-# if elif else
-#  mutually exclude
+        return False
 
-def binarySearchRecur(root,val):
-    if root is None: return False
-    if root.val == val: return True
+tree = BST()
+tree.insert(10)
+tree.insert(20)
+tree.insert(15)
+tree.insert(21)
+tree.insert(14)
 
-    elif val < root.val:
-        return binarySearchRecur(root.left,val)
-    else:
-        return binarySearchRecur(root.right,val)
+print(tree.binary_search_iter(14))
 
 
-print(binarySearchRecur(a,7), #true
-binarySearchRecur(a,5), #true
-binarySearchRecur(a,10), #true
-binarySearchRecur(a,16), #false
-binarySearchRecur(a,12)) #false
+# a = Node(10)
+# b = Node(5)
+# c = Node(15)
+# d = Node(3)
+# e = Node(7)
+# f = Node(17)
+
+# a.left = b
+# a.right = c
+# b.left = d
+# b.right = e
+# c.right = f
+
+# # if elif else
+# #  mutually exclude
+
+# def binarySearchRecur(root,val):
+#     if root is None: return False
+#     if root.val == val: return True
+
+#     elif val < root.val:
+#         return binarySearchRecur(root.left,val)
+#     else:
+#         return binarySearchRecur(root.right,val)
+
+
+# print(binarySearchRecur(a,7), #true
+# binarySearchRecur(a,5), #true
+# binarySearchRecur(a,10), #true
+# binarySearchRecur(a,16), #false
+# binarySearchRecur(a,12)) #false
 
 
 def binary_search_iter(root,val):
@@ -46,8 +89,8 @@ def binary_search_iter(root,val):
 
     return False
 
-print(binary_search_iter(a,7), #true
-binary_search_iter(a,5), #true
-binary_search_iter(a,10), #true
-binary_search_iter(a,16), #false
-binary_search_iter(a,12)) #false
+# print(binary_search_iter(a,7), #true
+# binary_search_iter(a,5), #true
+# binary_search_iter(a,10), #true
+# binary_search_iter(a,16), #false
+# binary_search_iter(a,12)) #false
