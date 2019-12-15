3f187ecf7beb757da59cbfc7123d45144d915c6a
John Daniele Capili <jdcapili12@gmail.com>
2019-12-15 16:02:55 -0500

    created tree

--- aAFightClub/12-15-19/bst.py
+++ aAFightClub/12-15-19/bst.py
@@ -4,5 +4,16 @@ class Node:
         self.left = None
         self.right = None
 
-node = Node(42)
-print(node.val)
+a = Node(10)
+b = Node(5)
+c = Node(15)
+d = Node(3)
+e = Node(7)
+f = Node(17)
+
+a.left = b
+a.right = c
+b.left = d
+b.right = e
+c.right = f
+
