fa076ed52eaae8e0d8fa6e8ce4b21ed2070e367a
John Daniele Capili <jdcapili12@gmail.com>
2019-12-17 12:56:01 -0500

    initial commit leetcode 139

---
+++ leetcode/139 word break.py
@@ -0,0 +1,11 @@
+var wordBreak = function(s, wordDict) {
+  let curStr = ""
+  let dictSet = new Set(wordDict);
+  for(let char of s){
+      if(dictSet.has(curStr)){
+          curStr = ""
+      }else{
+          curStr += char;
+      }
+  }
+};
