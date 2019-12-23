34a9d67b21bb7ecf7c1716a86b4bc64f5dc55cc2
John Daniele Capili <jdcapili12@gmail.com>
2019-12-23 15:08:40 -0500

    leetcode 814 alt solution

--- leetcode/814_binary_tree_pruning.py
+++ leetcode/814_binary_tree_pruning.py
@@ -9,3 +9,15 @@ class Solution:
             return None
         
         return root
+
+class Solution:
+    def pruneTree(self, root: TreeNode) -> TreeNode:
+        def containsOne(node):
+            if not node: return False
+            a1 = containsOne(node.left)
+            a2 = containsOne(node.right)
+            if not a1: node.left = None
+            if not a2: node.right = None
+            return node.val == 1 or a1 or a2
+
+        return root if containsOne(root) else None
