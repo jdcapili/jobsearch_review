3d7e9967cf01ad4f0461dfd777c71c80a71a4da3
John Daniele Capili <jdcapili12@gmail.com>
2020-01-08 17:46:42 -0500

    leetcode 139 memo solution

--- leetcode/139_wordbreak.py
+++ leetcode/139_wordbreak.py
@@ -10,3 +10,25 @@ class Solution:
                     tab[i+word_len] = True
                     
         return tab[-1]
+
+
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        dictSet = set(wordDict)
+        
+        def check(st,dictSet, memo={}):
+            if st in memo: return memo[st]
+            if not st: return True
+            curr = ""
+            for idx,char in enumerate(st):
+                curr += char
+                if curr in dictSet and check(st[idx+1::],dictSet):
+                    memo[curr] = True
+                    return memo[curr]
+
+                  
+            memo[st] = False
+            return memo[st]
+        
+        return check(s,dictSet)
+        
