b16ddc8b21bf2e931dc78e2626de659e3e0eb833
John Daniele Capili <jdcapili12@gmail.com>
2020-01-09 14:35:49 -0500

    pop,push,shift, and unshift methods

--- nyc-coders/doubly_linked_lists/dll.js
+++ nyc-coders/doubly_linked_lists/dll.js
@@ -1,11 +1,17 @@
 // abstract data types
-class Node{
-    constructor(val = null){
+// big O notation of DLL is worse than SLL but you can traverse through it better
+// import Node from "./node"
+
+class Node {
+    constructor(val = null) {
         this.value = val;
         this.next = null;
         this.prev = null;
     }
 }
+
+// a = new Node(5)
+// console.log(a.value)
 
 class DoublyLinkedLists{
     constructor(){
@@ -14,12 +20,67 @@ class DoublyLinkedLists{
         this.length = 0;
     }
 
-    push(){
+    push(val){
+        let node = new Node(val);
+        if(!this.head){
+            this.head = node;
+            this.tail = node;
+        }else{
+            this.tail.next = node;
+            node.prev = this.tail;
+            this.tail = node;
+        }
 
+        this.length ++;
+        return this
     }
 
     pop(){
+        let node = this.tail;
+        if(this.tail){
+            if(this.length > 1){
+                this.tail = node.prev;
+                this.tail.next = null;
+                node.prev = null;
+            }else{
+                this.tail = null;
+                this.head = null;
+            }
+            this.length --;
+        }
+        return node
+        
+    }
 
+    shift(){
+        let node = this.head;
+        if(!node){
+            return node
+        }else if(this.head === this.tail){
+            this.head = null
+            this.tail = null
+        }else{
+            this.head = node.next;
+            this.head.prev = null;
+            node.prev = null;
+        }
+        this.length --;
+        return node
+    }
+
+    unshift(val){
+        let node = new Node(val);
+        if(!this.head){
+            this.head = node;
+            this.tail = node;
+        }else{
+            node.next = this.head;
+            this.head.prev = node;
+            this.head = node;
+        }
+
+        this.length ++;
+        return this
     }
 
     insertAtIndex(){
@@ -38,3 +99,13 @@ class DoublyLinkedLists{
 
     }
 }
+
+dll = new DoublyLinkedLists();
+console.log(dll.push(2))
+console.log(dll.push(3))
+console.log(dll.pop())
+console.log(dll.unshift(3))
+console.log(dll.shift())
+console.log(dll)
+
+
---
+++ nyc-coders/doubly_linked_lists/node.js
@@ -0,0 +1,9 @@
+class Node {
+    constructor(val = null) {
+        this.value = val;
+        this.next = null;
+        this.prev = null;
+    }
+}
+
+export default Node
