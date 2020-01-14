db5946ee8d15c239250d603742e917c653d8430b
John Daniele Capili <jdcapili12@gmail.com>
2020-01-14 16:14:06 -0500

    created node class

--- algo_study/tries/uniquePrefixes.py
+++ algo_study/tries/uniquePrefixes.py
@@ -1,3 +1,11 @@
 # input = ["zebra", "dog", "duck", "dogs", "dove"]
 # output = ["z","dog", "du", "dogs", "dov"]
 
+class Node:
+    def __init__(self, char):
+        self.char = char
+        self.next = None
+        self.end_of_word = False
+
+class Trie:
+    
