2dd9757232c28991a5eb2cf4788f18db8f8b32f8
John Daniele Capili <jdcapili12@gmail.com>
2020-01-14 17:26:57 -0500

    created trie with print and insert

--- algo_study/tries/uniquePrefixes.py
+++ algo_study/tries/uniquePrefixes.py
@@ -2,10 +2,38 @@
 # output = ["z","dog", "du", "dogs", "dov"]
 
 class Node:
-    def __init__(self, char):
+    def __init__(self, char=""):
         self.char = char
-        self.next = None
+        self.children = {}
         self.end_of_word = False
 
 class Trie:
+    def __init__(self):
+        self.root = Node()
+        
+    def insert(self, string):
+        cur = self.root
+
+        for idx,char in enumerate(string):
+            if char in cur.children:
+                cur = cur.children[char]
+            else:
+                node = Node(char)
+                cur.children[char] = node
+                cur = cur.children[char]
+
+            if idx == len(string) -1: cur.end_of_word = True
+
+    def printChildren(self,root,cur=""):
+        if not root.children: 
+            print(cur + root.char)
+            return
+        cur += root.char
+        for child in root.children.values():
+            self.printChildren(child,cur)
     
+a = Trie()
+a.insert("zebra")
+a.insert("zygote")
+a.insert("dog")
+a.printChildren(a.root)
