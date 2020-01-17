d10d3afb85392d5f3f5a925b9964c9c25d0a9b7c
John Daniele Capili <jdcapili12@gmail.com>
2020-01-16 21:06:26 -0500

    modified print

--- algo_study/tries/uniquePrefixesV2.py
+++ algo_study/tries/uniquePrefixesV2.py
@@ -5,3 +5,37 @@ class Node:
     def __init__(self):
         self.children = {}
         self.isTerminal = False
+
+class Trie:
+    def __init__(self):
+        self.root = Node()
+
+    def insert(self, word):
+        cur = self.root
+        for idx,char in enumerate(word):
+            if char not in cur.children:
+                node = Node()
+                cur.children[char] = node
+
+            cur = cur.children[char]
+            if idx == len(word)-1: cur.isTerminal = True
+        
+
+        return self.root
+    
+    def printTrie(self, root,cur = ""):
+        print(cur)
+        if len(root.children) == 0: 
+            print(cur)
+            return
+        for child in root.children:
+            nxtCur = cur + child
+            nxtNode = root.children[child]
+            self.printTrie(nxtNode,nxtCur)
+
+test = Trie()
+test.insert("zebra")
+test.insert("zygote")
+test.insert("dog")
+test.insert("dove")
+test.printTrie(test.root)
