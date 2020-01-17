8800eb1b23a01c790fa7d7045bb8ccf4badb6c85
John Daniele Capili <jdcapili12@gmail.com>
2020-01-16 19:43:22 -0500

    node class

--- algo_study/tries/uniquePrefixesV2.py
+++ algo_study/tries/uniquePrefixesV2.py
@@ -1,2 +1,7 @@
 # input = ["zebra", "dog", "duck", "dogs", "dove"]
 #  output = ["z", "dog", "du", "dogs", "dov"]
+
+class Node:
+    def __init__(self):
+        self.children = {}
+        self.isTerminal = False
