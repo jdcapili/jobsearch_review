854fc3483fb611f63ae5f6689585dcabb8392a01
John Daniele Capili <jdcapili12@gmail.com>
2020-02-13 16:25:53 -0500

    leetcode 654

---
+++ leetcode/654_Maximum_binary_tree.py
@@ -0,0 +1,28 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def constructMaximumBinaryTree(self, nums: List[int]) -> TreeNode:
+        stack = []
+        last = None
+        
+        for num in nums:
+            while stack and stack[-1].val < num:
+                last = stack.pop()
+            
+            node = TreeNode(num)
+            
+            if stack:
+                stack[-1].right = node
+            if last:
+                node.left = last
+            
+            stack.append(node)
+            last = None
+        
+        return stack[0]
+            
